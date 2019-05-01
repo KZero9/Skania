a114c93360b8e3925197de78b3e7889a03f5132f
KZero9 <35094813+KZero9@users.noreply.github.com>
2019-05-01 12:54:23 +0900

    Update utility.js

--- js/utility.js
+++ js/utility.js
@@ -1,3 +1,33 @@
+function fullScreen() {
+  // Kind of painful, but this is how it works for now
+  if (document.documentElement.requestFullscreen) {
+    document.documentElement.requestFullscreen();
+  } else if (document.documentElement.mozRequestFullScreen) {
+    document.documentElement.mozRequestFullScreen();
+  } else if (document.documentElement.webkitRequestFullscreen) {
+    document.documentElement.webkitRequestFullscreen();
+  } else if (document.documentElement.msRequestFullscreen) {
+    document.documentElement.msRequestFullscreen();
+  }
+}
+
+function smolScreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  }
+}
+
+function lock(orientation) {
+  fullScreen();
+  screen.orientation.lock(orientation);
+}
+
 function hide() {
     document.getElementById("b1").style.visibility = "hidden";
     document.getElementById("b2").style.visibility = "hidden";
